58e299704042d70ec58790984a06e8a020827eb6
Fahad-alkamli <s3498306@student.rmit.edu.au>
2016-09-10 20:03:46 +1000

    Added listing new item page
    From this page we can start the process of creating new auction item

---
+++ listing.php
@@ -0,0 +1,232 @@
+
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title >Online Auction System</title>
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body style="padding-bottom: 70px;/*有底部固定导航条时设置*/">
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	    <div class="container">
+	        <div class="navbar-header">
+	            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	                <span class="sr-only">Toggle navigation</span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	            </button>
+	            <a class="navbar-brand" href="#" style="color:white">Auction System</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+
+                    <ul class="nav nav-pills pull-right" style="margin-top: 5px;">
+
+                        <li class="dropdown">
+ <p class="navbar-brand" href="#" style="color:white">Welcome '.$_SESSION['username'].'</p>';
+                          
+    
+                            <div class="dropdown-menu" style="padding:17px;">
+
+                            </div>
+                        </li>
+
+                    </ul>
+
+
+	        </div><!--/.navbar-collapse -->
+	    </div>
+	</nav>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	<div class="jumbotron" style="margin-top: 10px; margin-bottom: 10px;">
+	    <div class="container"  >
+            
+            
+            <!-- http://stackoverflow.com/questions/18153234/center-a-column-using-twitter-bootstrap-3 -->
+			<div class="row"  style="margin-top: 2em; " >
+				<div  class="col-md-2 col-md-offset-2">
+					<div >
+				<img src="http://thumbs.ebaystatic.com/images/g/1UwAAOSwdzVXvocB/s-l225.jpg" class="img-responsive img-thumbnail" alt="auction.jpg" style="height:18em; width:15em">
+					</div>
+				</div>
+                <div class="col-md-6 brd" >
+		
+                    
+                    <Div style="width:40em"> 
+                        <a href="http://www.ebay.com/itm/Wireless-Bluetooth-4-0-Headset-SPORT-Headphone-HiFi-Bass-Stereo-Earphone-Earbuds-/152230586050?hash=item2371a666c2:g:1UwAAOSwdzVXvocB" 
+                           onclick="alert('hello'); return false;"
+                           >
+                           Wireless Bluetooth 4.0 Headset SPORT Headphone HiFi Bass Stereo Earphone Earbuds
+                        </a>
+                    </Div>
+        
+                    <br/>
+                    
+                    <div>  
+                    <dl>
+                        <dt>Item Description:<dt/>
+                       <dd>This is a nice headphone with a wireless bla bla bla . <dd/>
+                    <dl/>
+                    </div>
+                        
+                    <div>
+                        <span style="font-weight: bold;">Auction Status:<span/>
+                        <span style="font-weight: normal;">Active <span/>
+                    </div>
+                    
+                            <br/>
+                    <div>
+                        <span style="font-weight: bold;">Minimum Bid:<span/>
+                        <span style="font-weight: normal;">$ 0<span/>
+                    </div>
+
+                    
+				</div>
+			</div>    
+    
+            
+            <!-- http://stackoverflow.com/questions/18153234/center-a-column-using-twitter-bootstrap-3 -->
+			<div class="row"  style="margin-top: 2em; " >
+				<div  class="col-md-2 col-md-offset-2">
+					<div >
+				<img src="http://thumbs.ebaystatic.com/images/g/1UwAAOSwdzVXvocB/s-l225.jpg" class="img-responsive img-thumbnail" alt="auction.jpg" style="height:18em; width:15em">
+					</div>
+				</div>
+                <div class="col-md-6 brd" >                 
+                    <Div style="width:40em"> 
+                        <a href="http://www.ebay.com/itm/Wireless-Bluetooth-4-0-Headset-SPORT-Headphone-HiFi-Bass-Stereo-Earphone-Earbuds-/152230586050?hash=item2371a666c2:g:1UwAAOSwdzVXvocB" 
+                           onclick="alert('hello'); return false;"
+                           >
+                           Wireless Bluetooth 4.0 Headset SPORT Headphone HiFi Bass Stereo Earphone Earbuds
+                        </a>
+                    </Div>
+        
+                    <br/>
+                    
+                    <div>  
+                    <dl>
+                        <dt>Item Description:<dt/>
+                       <dd>This is a nice headphone with a wireless bla bla bla . <dd/>
+                    <dl/>
+                    </div>
+                        
+                    <div>
+                        <span style="font-weight: bold;">Auction Status:<span/>
+                        <span style="font-weight: normal;">Active <span/>
+                    </div>
+                    
+                            <br/>
+                    <div>
+                        <span style="font-weight: bold;">Minimum Bid:<span/>
+                        <span style="font-weight: normal;">$ 0<span/>
+                    </div>
+
+                    
+				</div>
+			</div>    
+                                
+
+                                        
+            <!-- http://stackoverflow.com/questions/18153234/center-a-column-using-twitter-bootstrap-3 -->
+			<div class="row"  style="margin-top: 2em; " >
+				<div  class="col-md-2 col-md-offset-2">
+					<div >
+				<img src="http://thumbs.ebaystatic.com/images/g/1UwAAOSwdzVXvocB/s-l225.jpg" class="img-responsive img-thumbnail" alt="auction.jpg" style="height:18em; width:15em">
+					</div>
+				</div>
+                <div class="col-md-6 brd" >
+		
+                    
+                    <Div style="width:40em"> 
+                        <a href="http://www.ebay.com/itm/Wireless-Bluetooth-4-0-Headset-SPORT-Headphone-HiFi-Bass-Stereo-Earphone-Earbuds-/152230586050?hash=item2371a666c2:g:1UwAAOSwdzVXvocB" 
+                           onclick="alert('hello'); return false;"
+                           >
+                           Wireless Bluetooth 4.0 Headset SPORT Headphone HiFi Bass Stereo Earphone Earbuds
+                        </a>
+                    </Div>
+        
+                    <br/>
+                    
+                    <div>  
+                    <dl>
+                        <dt>Item Description:<dt/>
+                       <dd>This is a nice headphone with a wireless bla bla bla . <dd/>
+                    <dl/>
+                    </div>
+                        
+                    <div>
+                        <span style="font-weight: bold;">Auction Status:<span/>
+                        <span style="font-weight: normal;">Active <span/>
+                    </div>
+                    
+                            <br/>
+                    <div>
+                        <span style="font-weight: bold;">Minimum Bid:<span/>
+                        <span style="font-weight: normal;">$ 0<span/>
+                    </div>
+
+                    
+				</div>
+			</div>    
+    
+	    </div>
+	</div>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+<div class="container">
+<div class="row">
+    <div class="col-xs-12">
+        <footer class="navbar-fixed-bottom" style="background-color:black">
+            <p style="color: white">&copy; Copyright 2016 RMIT</p>
+        </footer>
+    </div>
+</div>
+</div>
+
+</body>
+</html>
